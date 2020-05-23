f8bb9cc21f1f0666107520b61d68eb8a01b3d0ae
maaayaa <civichann@gmail.com>
2020-05-23 18:39:50 +0900

    e

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const assert = require('assert');
 todo.todo('ノートを買う');
 todo.todo('鉛筆を買う');
 assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
-
+ 
 // done と donelist のテスト
 todo.done('鉛筆を買う');
 assert.deepEqual(todo.list(), ['ノートを買う']);
